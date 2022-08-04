4f07a10f3e85618bd88d3e9dc39219e8c405821c
Tomas <tomasgp1997@gmail.com>
2022-08-04 18:10:51 -0300

    117 - useState

---
+++ src/01-useState/CounterApp.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+export const CounterApp = () => {
+
+
+    const [counter, setCounter] = useState({
+        counter1: 10,
+        counter2: 20,
+        counter3: 30,
+    })
+
+    const changeCounter1 = () => {
+        setCounter( { ...counter, counter1: counter.counter1 + 1 } )
+    }
+    const changeCounter2 = () => {
+        setCounter( { ...counter, counter2: counter.counter2 + 1 } )
+    }
+    const changeCounter3 = () => {
+        setCounter( { ...counter, counter3: counter.counter3 + 1 } )
+    }
+
+
+  return (
+  
+    <>
+        <h1>Counter: { counter.counter1 }</h1>
+        <h1>Counter: { counter.counter2 }</h1>
+        <h1>Counter: { counter.counter3 }</h1>
+
+        <hr />
+
+        <button className="btn" onClick={ () => changeCounter1() }>+1</button>
+        <button className="btn" onClick={ () => changeCounter2() }>+1</button>
+        <button className="btn" onClick={ () => changeCounter3() }>+1</button>
+
+    </>
+
+  )
+}
+
+
+
+// export const CounterApp = () => {
+
+
+//     const [{counter1, counter2, counter3}, setCounter] = useState({
+//         counter1: 10,
+//         counter2: 20,
+//         counter3: 30,
+//     })
+
+ 
+
+
+//   return (
+  
+//     <>
+//         <h1>Counter: { counter1 }</h1>
+//         <h1>Counter: { counter2 }</h1>
+//         <h1>Counter: { counter3 }</h1>
+
+//         <hr />
+
+//         <button className="btn" onClick={ () => setCounter({ ...{counter1,counter2,counter3} , counter1: counter1 + 1 }) }>+1</button>
+//         <button className="btn" onClick={ () => setCounter({ ...{counter1,counter2,counter3} , counter2: counter2 + 1 }) }>+1</button>
+//         <button className="btn" onClick={ () => setCounter({ ...{counter1,counter2,counter3} , counter3: counter3 + 1 }) }>+1</button>
+
+//     </>
+
+//   )
+// }
---
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -0,0 +1,23 @@
+import { useCounter } from "../hooks/useCounter"
+
+export const CounterWithCustomHook = () => {
+
+
+    const { counter, increment, decrement, reset } = useCounter();
+
+
+
+  return (
+     <>
+     
+        <h1>Counter With Custom Hook: { counter }</h1>
+
+        <hr />
+
+        <button className="btn btn-primary" onClick={ () => increment() }>+1</button>
+        <button className="btn btn-primary" onClick={ reset }>Reset</button>
+        <button className="btn btn-primary" onClick={ () => decrement() }>-1</button>
+ 
+     </>
+  )
+}
